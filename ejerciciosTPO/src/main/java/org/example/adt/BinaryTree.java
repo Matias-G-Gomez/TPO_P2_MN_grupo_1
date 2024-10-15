5edfc4b99640948da89ca69968ca4506417496f1
Matias-G-Gomez <matiasgomez2012@gmail.com>
2024-10-15 12:10:46 -0300

    Agrego Clases e Interfaz de BinaryTree y Dictionary
    Agrego BinaryTree, DynamicBinaryTree, DynamicMultipleDictionary, MultipleDictionary, StaticMultipleDictionary
    Modifico StaticDictionary

---
+++ ejerciciosTPO/src/main/java/org/example/adt/BinaryTree.java
@@ -0,0 +1,11 @@
+package org.example.adt;
+
+public interface BinaryTree {
+    int getRoot();
+    void addLeft(int value);
+    void addRight(int value);
+    BinaryTree getLeft();
+    BinaryTree getRight();
+    void deleteLeft();
+    void deleteRight();
+}
